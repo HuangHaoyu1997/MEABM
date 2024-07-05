a8e01c3d8c996b677e35a8be0c7531e0d005d038
HuangHaoyu1997 <446700679@qq.com>
2024-07-05 11:48:15 +0800

    1

--- .history/main_20240626194356.py
+++
@@ -1,5 +0,0 @@
-class agent:
-    def __init__(self, pw:float, pc:float):
-        self.pw = pw # probability of working
-        self.w = 0 # wage
-        self.pc = pc # proportion consume
--- .history/main_20240626194443.py
+++
@@ -1,9 +0,0 @@
-class agent:
-    def __init__(self, pw:float, pc:float):
-        self.pw = pw # probability of working
-        self.w = 0 # wage
-        self.pc = pc # proportion of consumption
-        
-class bank:
-    def __init__(self, rn:float,):
-        self.rn = rn # 
